fc1aa7933983227cb749f4c6053918a5bf9e6ade
Matt <96960455+ForevaR@users.noreply.github.com>
2022-08-25 14:48:39 -0700

    - organised plots into better folders - saved catchment characteristic R script

---
+++ catchment_characteristic_plotting.R
@@ -0,0 +1,60 @@
+#########################
+## Name: Matt Watkins
+## Date: Aug. 25th / '22
+## Project: MSc data analysis
+## Objective: Make catchment characteristic plots to identify confounding variables
+## Inputs: ws_char RDS
+## Outputs: ggplots
+#########################
+
+## 0. NOTES ----
+
+## 1. PREPARE ----
+
+rm(list=ls())
+options(stringsASfactors = FALSE, scipen = 999, encoding = "UTF-8")
+
+library(tidyverse)
+library(readxl)
+library(rlang)
+
+## 2. IMPORT ----
+
+ws_char <- readRDS("watershed_characteristics_cleaned.RDS")
+
+## 3. TIDY // PROCESS ----
+
+ws_wide <- ws_char %>% 
+  spread(variable, value) %>% 
+  select(-site, -catchment.id)# make data wide format to setup for auto graphs
+
+response <- names(ws_wide)[11]
+expl <- names(ws_wide)[1:10]
+
+## 4. PLOTTING ----
+
+### 4.01 - Plot the type of graph I want once
+
+ws_wide %>% 
+  ggplot(aes(area, bog)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y~x, se = FALSE, colour = "black") +
+  scale_x_log10() +
+  labs(x = "Area", y = "Bog (%)") # standard area-bog scatter plot
+
+### 4.02 - Change this to a function to automate similar graphs
+
+expl_plots <- function(x, y) {
+    ggplot(ws_wide, aes(x = .data[[x]], y = .data[[y]])) +
+    geom_point() +
+    geom_smooth(method = "lm", formula = y~x, se = FALSE, colour = "black") +
+    scale_x_log10()
+}
+
+expl_plots(x = "area", y = "wetland") # test the function - it works
+
+
+## 5. SAVING // EXPORTING ----
+
+## 6. TRIAL // JUNK CODE ----
+
--- initial_doc_exploration.R
+++ initial_doc_exploration.R
@@ -33,16 +33,12 @@ chem_out <- chem %>%
 doc <- chem_out %>% 
   subset(variable == "organic.carbon")
 
-### 3.02 - Connect DOC and catchment area
+### 3.02 - Connect DOC and catchment characteristics
 
-area <- ws_char %>% 
-  subset(variable == "area")
-
-area_join <- left_join(area, doc, by = c("site", "catchment.id")) %>% # join by similar columns
+ws_doc <- left_join(ws_char, doc, by = c("site","catchment.id")) %>% 
   group_by(site) %>% 
-  mutate(mean.doc = mean(value.y)) # take the mean DOC for each site
-  
-  
+  mutate(mean.doc = mean(value.y))
+
 ## 4. PLOTTING ----
 
 ### 4.01 - Look at DOC variability across time for all sites
@@ -51,7 +47,13 @@ doc %>%
   ggplot(aes(date, value)) +
   geom_point() +
   facet_wrap(~ catchment.id) +
-  labs(x = "", y = "DOC (mg/L)")
+  labs(x = "", y = "DOC (mg/L)") # point plot
+
+doc %>% 
+  ggplot(aes(date, value)) +
+  geom_line() +
+  facet_wrap(~ catchment.id) +
+  labs(x = "", y = "DOC (mg/L)") # line plot variation
 
 ### 4.02 - DOC and catchment area
 
@@ -64,6 +66,46 @@ area_join %>%
   #geom_smooth(method='lm', formula= y~x)
 
 # add labels - label = catchment.id
+
+### 4.03 - DOC and slope
+
+slope_j %>% 
+  ggplot(aes(value.x, mean.doc)) +
+  #geom_text(hjust = 0, vjust = 0) +
+  geom_smooth(method = "lm", formula = y~x, se = FALSE, colour = "black") +
+  geom_point() +
+  labs(x = "Slope (%)", y = "DOC (ppm)")
+
+### 4.04 - DOC and latitude
+
+lat <- ws_doc[ws_doc$variable.x == "latitude" & ws_doc$mean.doc, ]
+
+lat %>% 
+ggplot(aes(value.x, mean.doc)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y~x, se = FALSE, colour = "black") +
+  labs(x = "Latitude", y = "DOC (mg/L)")
+
+### 4.05 - DOC and latitude
+
+long <- ws_doc[ws_doc$variable.x == "longitude" & ws_doc$mean.doc, ]
+
+long %>% 
+  ggplot(aes(value.x, mean.doc)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y~x, se = FALSE, colour = "black") +
+  labs(x = "Longitude", y = "DOC (mg/L)")
+
+### 4.06 - DOC and latitude
+
+elev <- ws_doc[ws_doc$variable.x == "elevation" & ws_doc$mean.doc, ]
+
+elev %>% 
+  ggplot(aes(value.x, mean.doc)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y~x, se = FALSE, colour = "black") +
+  labs(x = "Elevation (masl)", y = "DOC (mg/L)")
+
 
 ## 5. SAVING // EXPORTING ----
 
