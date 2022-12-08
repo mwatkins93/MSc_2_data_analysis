108ee0c431bf333e39feed2c870f6680865ca651
Matt <96960455+ForevaR@users.noreply.github.com>
2022-12-07 20:26:02 -0500

    upd - corrected WS 47 calculations (max/min + regression only)

--- doc_load_estimation.R
+++ doc_load_estimation.R
@@ -43,28 +43,28 @@ mass_loads <- read_xlsx("doc_load_estimates.xlsx")
 
 setwd("/Volumes/MW/2020 Trent University/R/Thesis Data/Water_level_correlation/Discharge Estimates")
 
-ws52_dailyQ <- readRDS("ws52_dailyQ.rds")
+ws47_dailyQ <- readRDS("ws47_dailyQ.rds")
 
-ws52_chem <- wtr_chem %>% 
+ws47_chem <- wtr_chem %>% 
   select(-glfc.id) %>%
-  filter(site %in% "WS 52" & variable %in% "organic.carbon") %>% 
+  filter(site %in% "WS 47" & variable %in% "organic.carbon") %>% 
   separate(date, c("year", "month", "day"), sep = "(\\-| )") %>% 
   select(-year)
 
-ws52_out <- left_join(ws52_dailyQ, ws52_chem) %>% 
+ws47_out <- left_join(ws47_dailyQ, ws47_chem) %>% 
   mutate(Q.litres = dailyQ * 1000) # This process works and also converts Q to L/s so I can obtain mass flux in mg/s
 
 ### 3.03 - Load range mass flux computation ----
 
 #### 3.03.1 - Check range of DOC ----
 
-range(ws52_out$value, na.rm = TRUE)
+range(ws47_out$value, na.rm = TRUE)
 
 #### 3.03.2 - DOC flux range calculation: high and low ----
 
-ws52_loadrange <- ws52_out %>% # making sure EVERY conversion is correct
-  mutate(top.mass.flux = (Q.litres * 12.968),
-         btm.mass.flux = (Q.litres * 8.678),
+ws47_loadrange <- ws47_out %>% # making sure EVERY conversion is correct
+  mutate(top.mass.flux = (Q.litres * 11.670),
+         btm.mass.flux = (Q.litres * 7.064),
          top.mg.min = (top.mass.flux * 60),
          btm.mg.min = (btm.mass.flux * 60),
          top.mg.hour = (top.mg.min * 60),
@@ -72,16 +72,16 @@ ws52_loadrange <- ws52_out %>% # making sure EVERY conversion is correct
          top.mg.day = (top.mg.hour * 24),
          btm.mg.day = (btm.mg.hour * 24))
 
-ws52_timeframe <- ws52_loadrange[-c(1, 2, 3, 4, 5, 6, 7, 8), ] # remove the rows outside of the set timeframe
+ws47_timeframe <- ws47_loadrange[-c(1, 2, 3, 4, 5, 6, 7, 8), ] # remove the rows outside of the set timeframe
 
-ws52.top.mg.szn <- sum(ws52_timeframe$top.mg.day) # sum the max amount for the study period
-ws52.btm.mg.szn <- sum(ws52_timeframe$btm.mg.day) # sum the min amount for the study period
+ws47.top.mg.szn <- sum(ws47_timeframe$top.mg.day) # sum the max amount for the study period
+ws47.btm.mg.szn <- sum(ws47_timeframe$btm.mg.day) # sum the min amount for the study period
 
-ws52.top.mgC.per.km2.szn <- ws52.top.mg.szn / 1.2 # standardise for catchment area
-ws52.btm.mgC.per.km2.szn <- ws52.btm.mg.szn / 1.2
+ws47.top.mgC.per.km2.szn <- ws47.top.mg.szn / 19.6 # standardise for catchment area
+ws47.btm.mgC.per.km2.szn <- ws47.btm.mg.szn / 19.6
   
-ws52.top.kgC.per.km2.szn <- ws52.top.mgC.per.km2.szn / 1000000 # convert to kg C / km2 * szn
-ws52.btm.kgC.per.km2.szn <- ws52.btm.mgC.per.km2.szn / 1000000
+ws47.top.kgC.per.km2.szn <- ws47.top.mgC.per.km2.szn / 1000000 # convert to kg C / km2 * szn
+ws47.btm.kgC.per.km2.szn <- ws47.btm.mgC.per.km2.szn / 1000000
 
 ######### Repeatable process for the other watersheds ################
 ######################################################################
@@ -105,23 +105,23 @@ ws87.int.kgC.per.km2.szn <- ws87.int.mgC.per.km2.szn / 1000000
 
 ### 3.05 - linear regression ----
 
-ws52_cQ <- lm(value ~ dailyQ, data = ws52_out) # make a model from the 6 manual Q and concurrent concentrations
+ws47_cQ <- lm(value ~ dailyQ, data = ws47_out) # make a model from the 6 manual Q and concurrent concentrations
 
-summary(ws52_cQ)
+summary(ws47_cQ)
 
 #### Manually predict concentrations based on regression
 
-ws52_loadrange$reg.predict <- ws52_cQ$coef[1] + ws52_cQ$coef[2]*ws52_loadrange$dailyQ
+ws47_loadrange$reg.predict <- ws47_cQ$coef[1] + ws47_cQ$coef[2]*ws47_loadrange$dailyQ
 
-ws52_reg_mf <- ws52_loadrange %>% 
+ws47_reg_mf <- ws47_loadrange %>% 
   mutate(reg.mass.flux = (Q.litres * reg.predict))
 
-ws52_reg_timeframe <- ws52_reg_mf[-c(1, 2, 3, 4, 5, 6, 7, 8), ] %>% 
+ws47_reg_timeframe <- ws47_reg_mf[-c(1, 2, 3, 4, 5, 6, 7, 8), ] %>% 
   mutate(reg.mg.day = (reg.mass.flux * 86400))
 
-ws52.reg.mgC.per.km2.szn <- sum(ws52_reg_timeframe$reg.mg.day) / 1.2
+ws47.reg.mgC.per.km2.szn <- sum(ws47_reg_timeframe$reg.mg.day) / 19.6
 
-ws52.reg.kgC.per.km2.szn <- ws52.reg.mgC.per.km2.szn / 1000000
+ws47.reg.kgC.per.km2.szn <- ws47.reg.mgC.per.km2.szn / 1000000
 
 
 
