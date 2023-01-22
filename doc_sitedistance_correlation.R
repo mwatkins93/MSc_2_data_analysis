a713078b3fb9f05751dfdbfd274db2920633fe4b
Matt <96960455+ForevaR@users.noreply.github.com>
2023-01-22 18:59:26 -0500

    upd - adjusted excel table sheet 3; added new script for site distance/doc correlation.

---
+++ doc_sitedistance_correlation.R
@@ -0,0 +1,32 @@
+#########################
+## Name: Matt Watkins
+## Date: Jan. 22nd / 23
+## Project: MSc data analysis
+## Objective: DOC Correlation and distance relationship
+## Inputs:
+## Outputs:
+#########################
+
+## 0. NOTES ----
+
+## 1. PREPARE ----
+
+rm(list=ls())
+options(stringsASfactors = FALSE, scipen = 999, encoding = "UTF-8")
+
+library(tidyverse)
+library(readxl)
+
+## 2. IMPORT ----
+
+site_pairs <- read_excel("~/Desktop/2020 Trent University/R/Thesis Data/MSc_data_analysis/Watershed_table_v1.xlsx", sheet = 3)
+
+
+## 3. TIDY // PROCESS ----
+
+## 4. PLOTTING ----
+
+## 5. SAVING // EXPORTING ----
+
+## 6. TRIAL // JUNK CODE ----
+
