1aad5a17d55d3c86fa288e20f9573da53e57702b
Matt <96960455+ForevaR@users.noreply.github.com>
2023-01-31 20:17:28 -0500

    upd - exploratory graph now completed. Seems to be a bit problematic; thoughts: (1) potentially removing the sites with missing samples earlier would be better; (2) using a different method for the spearman correlation?
    Anyway, this version is sent off the Jason and I'll wait for feedback.

--- doc_sitedistance_correlation.R
+++ doc_sitedistance_correlation.R
@@ -14,12 +14,9 @@
 rm(list=ls())
 options(stringsASfactors = FALSE, scipen = 999, encoding = "UTF-8")
 
-install.packages("ggtree")
-
 library(tidyverse)
 library(readxl)
 library(geosphere)
-library(harrietr)
 
 ## 2. IMPORT ----
 
@@ -28,6 +25,8 @@ ws_table <- read_excel("~/Desktop/2020 Trent University/R/Thesis Data/MSc_data_a
 water_chem <- readRDS("glfc_chem_cleaned_v1.00.rds")
 
 ## 3. TIDY // PROCESS ----
+
+### 3.01 - Find a way to make a distance matrix for site to site distances
 
 distm(c(-84.1009, 46.7460), c(-83.8506, 46.7571), fun = distHaversine) # this calculates the distance between two different lat/long points.
 
@@ -38,26 +37,77 @@ site_latlong <- site_latlong[, c(1, 2, 4, 3)] # rearrange columns for geosphere 
 
 site_dist_mtx<- distm(site_latlong[3:4], fun = distHaversine) # computes all distances between sites
 
-rownames(site_dist_mtx) = site_latlong$`Site name`
-colnames(site_dist_mtx) = site_latlong$`Site name`
+rownames(site_dist_mtx) = site_latlong$`Catchment ID` # change row and column names so I can set up site pairs later
+colnames(site_dist_mtx) = site_latlong$`Catchment ID`
 
-### 3.01 - Adjust doc in prep for correlation
+#### 3.01.1 - Reshape the matrix to a pairwise dataframe (site)
+
+euclid_df <- melt(site_dist_mtx)[melt(upper.tri(site_dist_mtx))$value,] # reshape the matrix into a dataframe with three columns
+
+colnames(euclid_df) <- c("site.one", "site.two", "euclidean.distance") # rename the columns
+
+euclid_df$site.pair <- str_c(euclid_df$site.one, euclid_df$site.two, sep = "-")
+
+euclid_df <- euclid_df[, c(1, 2, 4, 3)]
+
+### 3.02 - Adjust doc in prep for correlation
   
 doc <- water_chem %>% 
 select(-glfc.id, -date) %>% 
   filter(variable %in% "organic.carbon") %>% 
   pivot_wider(names_from = "sample", values_from = "value")
 
-doc_corr_mtx <- cor(t(doc[4:9]), method="spearman", use = "pairwise.complete.obs") # all correlations between rows completed
+doc_shift <- doc[30:1, ]
 
-rownames(doc_corr_mtx) = doc$site
-colnames(doc_corr_mtx) = doc$site
+doc_ordered <- doc_shift[c(9, 1:5, 7, 8, 6, 10:13, 15, 14, 16, 17, 26:18, 27:30),]
 
+doc_corr_mtx <- cor(t(doc_ordered[4:9]), method="spearman", use = "pairwise.complete.obs") # all correlations between rows completed
 
+rownames(doc_corr_mtx) = doc_ordered$catchment.id
+colnames(doc_corr_mtx) = doc_ordered$catchment.id
+
+#### 3.02.1 - Reshape the doc matrix to a pairwise dataframe (site)
+
+doc_corr_df <- melt(doc_corr_mtx)[melt(upper.tri(doc_corr_mtx))$value,] # reshape the matrix into a dataframe with three columns
+
+colnames(doc_corr_df) <- c("site.one", "site.two", "correlation.coeff") # rename the columns
+
+doc_corr_df$site.pair <- str_c(doc_corr_df$site.one, doc_corr_df$site.two, sep = "-")
+
+doc_corr_df <- doc_corr_df[, c(1, 2, 4, 3)]
+
+doc_corr_out <- doc_corr_df %>% 
+  select(site.pair, correlation.coeff)
+
+### 3.03 - Join the dataframes to have the ideal output - site.one, site.two, site.pair, euclid.dist, corr.coeff
+
+dist_coeff_out <- left_join(doc_corr_out, euclid_df, by = "site.pair")
+
+dist_coeff_out <- dist_coeff_out[, c(3, 4, 1, 5, 2)]
 
 ## 4. PLOTTING ----
 
+### 4.01 - Plot the general plot of everything
+
+dist_coeff_out %>% 
+  ggplot(aes(x = euclidean.distance, y = correlation.coeff)) +
+  geom_point() +
+  theme_bw()
+
+### 4.02 - What if we remove the sites with missing samples (C13 and C10)
+
+dist_coeff_out %>% 
+  filter(!site.one %in% c("C13", "C10") & !site.two %in% c("C13", "C10")) %>% 
+  ggplot(aes(x = euclidean.distance, y = correlation.coeff)) +
+  geom_point() +
+  theme_bw()
+
 ## 5. SAVING // EXPORTING ----
+
+### 5.01 - Save distance-coefficient df
+
+dist_coeff_out %>% 
+  saveRDS(file = "distance_coeff_clean.rds")
 
 ## 6. TRIAL // JUNK CODE ----
 
