83c3b1286a772a498a23ecaefd080d987fa96bff
Matt <96960455+ForevaR@users.noreply.github.com>
2023-02-03 13:41:33 -0500

    upd - removed mixed forest column from watershed table. Threw everything (almost all variables) at the model and selectively removed some variables that made sense.

--- doc_mean_multiple_regression.R
+++ doc_mean_multiple_regression.R
@@ -35,15 +35,19 @@ mean_doc <- doc_table %>%
 
 doc_ws_table <- left_join(mean_doc, ws_table, by = "Site name")
   
-doc_mregression <- lm(Mean ~ `Drainage Area (km2)` + `Elevation (m a.s.l.)` + `Slope (degrees)` + `Wetland Cover (%)` + `Open Water (%)` + `Total Productive Forest (%)` + `Deciduous Forest (%)` + `Coniferous Forest (%)` + `20-year Harvest Disturbance (%)` + Latitude + `15-year Insect Disturbance (%)`, data = doc_ws_table) # run the model with a bunch of variables for now
+doc_mregression <- lm(Mean ~ `Drainage Area (km2)` + Latitude + Longitude + `Elevation (m a.s.l.)` + `Slope (degrees)` + `Wetland Cover (%)` + `Open Water (%)` + `Total Productive Forest (%)` + `Deciduous Forest (%)` + `Coniferous Forest (%)` + `5-year Harvest Disturbance (%)` + `5-year Insect Disturbance (%)` + `5-year Abiotic Disturbance (%)` + `10-year Harvest Disturbance (%)` + `10-year Insect Disturbance (%)` + `15-year Harvest Disturbance (%)` + `15-year Insect Disturbance (%)` + `15-year Wildfire Disturbance (%)` + `20-year Harvest Disturbance (%)` + `20-year Insect Disturbance (%)`, data = doc_ws_table) # run the model with almost all of the variables for now
 
-summary(doc_mregression) # check out the statistics
+# Variables that have been selectively removed:
+# 1. 5-year wildfire - no sites affected
+# 2. 10-year wildfire - only one site affected
+# 3. 10-year abiotic - same as 5-year
+# 4. 15-year abiotic - same as 5-year
+# 5. 20-year abiotic - same as 5-year
+# 6. 20-year wildfire - the same percentage as 15-year wildfire
 
-check_model(doc_mregression) # check the model performance based on the requirements - 
+summary(doc_mregression) # check out the statistics - p-value = 0.16 (all of these variables don't help explain mean [DOC] that much; multiple r2 = 0.81, pretty decent amount of variability explained, which makes sense - this is the kitchen sink)
 
-
-
-
+check_model(doc_mregression) # check the model performance based on the requirements - some problems here (extreme collinearity - likely the temporal disturbance, one outlier, linearity, homogeneity of variance and residual normality not obeyed). This is good because it really can only improve from here.
 
 ## 4. PLOTTING ----
 
