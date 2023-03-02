5d216c665415c4e32a0dfaeca27c51550fb7bb1f
Matt <96960455+ForevaR@users.noreply.github.com>
2023-03-02 13:28:20 -0500

    upd - section 3.04 set up for wetland only inclusion coefficient plot

--- std_model_coeff_plot.R
+++ std_model_coeff_plot.R
@@ -178,26 +178,26 @@ doc_tbl_no_c3 <- standard_doc_tbl %>%
   filter(!`Catchment ID` %in% "C3") # df with removal of C3
 
 #### 3.04.1 - Mean DOC ----
-mean_base_model <- lm(mean.doc ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st, data = doc_tbl_no_c3)
+mean_base_model <- lm(mean.doc ~ Group + conifer_st + drainage_st + elev_st + wetland_st, data = standard_doc_tbl)
 
-summary(mean_base_model)
-check_model(mean_base_model)
+# summary(mean_base_model)
+# check_model(mean_base_model)
 
 mean_base_model_tbl <- dredge(mean_base_model, rank = "AICc")
 
-mean_base_model_avg <- model.avg(subset(mean_base_model_tbl, delta <= 2.3, recalc.weights=FALSE), fit = TRUE)
+mean_base_model_avg <- model.avg(subset(mean_base_model_tbl, delta <= 2, recalc.weights=FALSE), fit = TRUE)
 
 #### 3.04.2 - Sample campaign 1 ----
 
-sc1_base_model <- lm(doc.s1 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st, data = doc_tbl_no_c3)
+sc1_base_model <- lm(doc.s1 ~ Group + conifer_st + drainage_st + elev_st + wetland_st, data = standard_doc_tbl)
 
 sc1_base_table <- dredge(sc1_base_model, rank = "AICc")
 
-sc1_base_model_avg <- model.avg(subset(sc1_base_table, delta <= 2.2, recalc.weights = FALSE), fit = TRUE)
+sc1_base_model_avg <- model.avg(subset(sc1_base_table, delta <= 2, recalc.weights = FALSE), fit = TRUE)
 
 #### 3.04.3 - Sample campaign 2 ----
 
-sc2_base_model <- lm(doc.s2 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st, data = doc_tbl_no_c3)
+sc2_base_model <- lm(doc.s2 ~ Group + conifer_st + drainage_st + elev_st + wetland_st, data = standard_doc_tbl)
 
 sc2_base_table <- dredge(sc2_base_model, rank = "AICc")
 
@@ -205,9 +205,9 @@ sc2_base_model_avg <- model.avg(subset(sc2_base_table, delta <= 2, recalc.weight
 
 #### 3.04.4 - Sample campaign 3 ----
 
-doc_s3_sub <- doc_tbl_no_c3[-3, c(1:53, 56)]
+doc_s3_sub <- standard_doc_tbl[-3, c(1:53, 56)]
 
-sc3_base_model <- lm(doc.s3 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st, data = doc_s3_sub)
+sc3_base_model <- lm(doc.s3 ~ Group + conifer_st + drainage_st + elev_st + wetland_st, data = doc_s3_sub)
 
 sc3_base_table <- dredge(sc3_base_model, rank = "AICc")
 
@@ -215,9 +215,9 @@ sc3_base_model_avg <- model.avg(subset(sc3_base_table, delta <= 2, recalc.weight
 
 #### 3.04.5 - Sample campaign 4 ----
 
-doc_s4_sub <- doc_tbl_no_c3[c(-3, -6), c(1:53, 57)]
+doc_s4_sub <- standard_doc_tbl[c(-3, -6), c(1:53, 57)]
 
-sc4_base_model <- lm(doc.s4 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st, data = doc_s4_sub)
+sc4_base_model <- lm(doc.s4 ~ Group + conifer_st + drainage_st + elev_st + wetland_st, data = doc_s4_sub)
 
 sc4_base_table <- dredge(sc4_base_model, rank = "AICc")
 
@@ -225,9 +225,9 @@ sc4_base_model_avg <- model.avg(subset(sc4_base_table, delta <= 2, recalc.weight
 
 #### 3.04.6 - Sample campaign 5 ----
 
-doc_s5_sub <- doc_tbl_no_c3[-3, c(1:53, 58)]
+doc_s5_sub <- standard_doc_tbl[-3, c(1:53, 58)]
 
-sc5_base_model <- lm(doc.s5 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st, data = doc_s5_sub)
+sc5_base_model <- lm(doc.s5 ~ Group + conifer_st + drainage_st + elev_st + wetland_st, data = doc_s5_sub)
 
 sc5_base_table <- dredge(sc5_base_model, rank = "AICc")
 
@@ -235,7 +235,7 @@ sc5_base_model_avg <- model.avg(subset(sc5_base_table, delta <= 2, recalc.weight
 
 #### 3.04.7 - Sample campaign 6 ----
 
-sc6_base_model <- lm(doc.s6 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st, data = doc_tbl_no_c3)
+sc6_base_model <- lm(doc.s6 ~ Group + conifer_st + drainage_st + elev_st + wetland_st, data = standard_doc_tbl)
 
 sc6_base_table <- dredge(sc6_base_model, rank = "AICc")
 
@@ -319,7 +319,7 @@ mean_base_model_plot <- dwplot(base_model_avgs) %>%
            GroupInsect = "Insect Class",
            GroupMixed = "Mixed Class")) +
   theme_bw() +
-  labs(title = "Average multiple regression coefficients - no C3") +
+  labs(title = "Average multiple regression coefficients - wetland only") +
   theme(legend.title = element_blank(), plot.title = element_text(hjust = 0.5, face="bold")) +
   geom_vline(xintercept = 0) +
   scale_colour_manual(labels = c("SC1", "SC2", "SC3", "SC4", "SC5", "SC6", "Mean DOC"),
