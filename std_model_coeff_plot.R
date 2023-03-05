4d559f2debeb2d32862bdacdbe819dcf1b6a1efb
Matt <96960455+ForevaR@users.noreply.github.com>
2023-03-05 18:01:28 -0500

    upd:
    1. section 3.04 is 10-year harvest coeff plot
    2. section 6.04 is updated to show correlations and scatterplots between disturbance class time blocks (harvest and infestation)

--- std_model_coeff_plot.R
+++ std_model_coeff_plot.R
@@ -181,7 +181,7 @@ doc_tbl_no_c3 <- standard_doc_tbl %>%
   filter(!`Catchment ID` %in% "C3") # df with removal of C3
 
 #### 3.04.1 - Mean DOC ----
-mean_base_model <- lm(mean.doc ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st + harv15_st, data = standard_doc_tbl)
+mean_base_model <- lm(mean.doc ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st + harv10_st, data = standard_doc_tbl)
 
 # summary(mean_base_model)
 # check_model(mean_base_model)
@@ -192,7 +192,7 @@ mean_base_model_avg <- model.avg(subset(mean_base_model_tbl, delta <= 2, recalc.
 
 #### 3.04.2 - Sample campaign 1 ----
 
-sc1_base_model <- lm(doc.s1 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st + harv15_st, data = standard_doc_tbl)
+sc1_base_model <- lm(doc.s1 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st + harv10_st, data = standard_doc_tbl)
 
 sc1_base_table <- dredge(sc1_base_model, rank = "AICc")
 
@@ -200,7 +200,7 @@ sc1_base_model_avg <- model.avg(subset(sc1_base_table, delta <= 2, recalc.weight
 
 #### 3.04.3 - Sample campaign 2 ----
 
-sc2_base_model <- lm(doc.s2 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st + harv15_st, data = standard_doc_tbl)
+sc2_base_model <- lm(doc.s2 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st + harv10_st, data = standard_doc_tbl)
 
 sc2_base_table <- dredge(sc2_base_model, rank = "AICc")
 
@@ -210,7 +210,7 @@ sc2_base_model_avg <- model.avg(subset(sc2_base_table, delta <= 2, recalc.weight
 
 doc_s3_sub <- standard_doc_tbl[-3, c(1:53, 56)]
 
-sc3_base_model <- lm(doc.s3 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st + harv15_st, data = doc_s3_sub)
+sc3_base_model <- lm(doc.s3 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st + harv10_st, data = doc_s3_sub)
 
 sc3_base_table <- dredge(sc3_base_model, rank = "AICc")
 
@@ -220,7 +220,7 @@ sc3_base_model_avg <- model.avg(subset(sc3_base_table, delta <= 2, recalc.weight
 
 doc_s4_sub <- standard_doc_tbl[c(-3, -6), c(1:53, 57)]
 
-sc4_base_model <- lm(doc.s4 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st + harv15_st, data = doc_s4_sub)
+sc4_base_model <- lm(doc.s4 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st + harv10_st, data = doc_s4_sub)
 
 sc4_base_table <- dredge(sc4_base_model, rank = "AICc")
 
@@ -230,7 +230,7 @@ sc4_base_model_avg <- model.avg(subset(sc4_base_table, delta <= 2, recalc.weight
 
 doc_s5_sub <- standard_doc_tbl[-3, c(1:53, 58)]
 
-sc5_base_model <- lm(doc.s5 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st + harv15_st, data = doc_s5_sub)
+sc5_base_model <- lm(doc.s5 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st + harv10_st, data = doc_s5_sub)
 
 sc5_base_table <- dredge(sc5_base_model, rank = "AICc")
 
@@ -238,7 +238,7 @@ sc5_base_model_avg <- model.avg(subset(sc5_base_table, delta <= 2, recalc.weight
 
 #### 3.04.7 - Sample campaign 6 ----
 
-sc6_base_model <- lm(doc.s6 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st + harv15_st, data = standard_doc_tbl)
+sc6_base_model <- lm(doc.s6 ~ Group + conifer_st + drainage_st + elev_st + open_wat_st + wetland_st + slope_st + harv10_st, data = standard_doc_tbl)
 
 sc6_base_table <- dredge(sc6_base_model, rank = "AICc")
 
@@ -324,9 +324,10 @@ mean_base_model_plot <- dwplot(base_model_avgs) %>%
            GroupInsect = "Insect Class",
            GroupMixed = "Mixed Class",
            harv20_st = "20-year Harvest",
-           harv15_st = "15-year Harvest")) +
+           harv15_st = "15-year Harvest",
+           harv10_st = "10-year Harvest")) +
   theme_bw() +
-  labs(title = "Average multiple regression coefficients - 15-year Harvest") +
+  labs(title = "Average multiple regression coefficients - 10-year Harvest") +
   theme(legend.title = element_blank(), plot.title = element_text(hjust = 0.5, face="bold")) +
   geom_vline(xintercept = 0) +
   scale_colour_manual(labels = c("SC1", "SC2", "SC3", "SC4", "SC5", "SC6", "Mean DOC"),
@@ -402,12 +403,42 @@ cor(standard_doc_tbl$`Deciduous Forest (%)`, standard_doc_tbl$`Coniferous Forest
 # 0.31 deciduous and total productive forest
 # -.58 conifer and deciduous
 
-### 6.04 - Look at harvest scatterplots and correlations ----
+### 6.04 - Look at harvest/infestation scatterplots and correlations ----
 
 standard_doc_tbl %>% 
-  ggplot(aes(x = `15-year Harvest Disturbance (%)`, y = `20-year Harvest Disturbance (%)`)) +
-  geom_point() # strong linearity
+  ggplot(aes(x = `20-year Insect Disturbance (%)`, y = `5-year Insect Disturbance (%)`)) +
+  geom_point()
 
-cor(standard_doc_tbl$`20-year Harvest Disturbance (%)`, standard_doc_tbl$`15-year Harvest Disturbance (%)`, method = "spearman") # 0.86 - strongly correlated!
+# Harvest
+
+# 15 and 20 year: strong linearity
+# 15 and 10 year: strong linearity
+# 15 and 5 year: strong linearity (less that 15 and 10)
+# 10 and 5 year: strong linearity
+
+# Infestation
+
+# 20 and 15 year: appears linear but not due to a large number of 0% sites at 15-year that are 100% at 20-year
+# 20 and 5-year: no relationship
+# 15 and 10-year: strongly linear
+# 15 and 5-year: no relationship
+# 10 and 5-year: no relationship
+
+
+cor(standard_doc_tbl$`15-year Insect Disturbance (%)`, standard_doc_tbl$`5-year Insect Disturbance (%)`, method = "spearman")
+
+# Harvest
+
+# 15 and 20 year: 0.86 - strongly correlated!
+# 15 and 10 year: 0.94 - strongly correlated
+# 15 and 5 year: 0.77 - still quite strong
+# 10 and 5 year: 0.84 - strongly correlated
+
+# Infestation
+
+# 20 and 15 year: -0.44
+# 20 and 5 year: -0.33
+# 15 and 10 year: 1 (perfect correlation)
+# 10 and 5 year: 0.75
 
 
